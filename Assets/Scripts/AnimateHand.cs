8221d1ee692ea6ee56ce50dff1b3b0e84716811b
Guilherme Saturno <guilhermebsaturno@gmail.com>
2023-12-19 19:09:34 +0000

    Add custom hand and default animations

---
+++ Assets/Scripts/AnimateHand.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHand : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty pinchAnimation;
+    [SerializeField] private InputActionProperty fistAnimation;
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float triggerValue = pinchAnimation.action.ReadValue<float>();
+        float gripValue = fistAnimation.action.ReadValue<float>();
+
+        animator.SetFloat("Trigger", triggerValue);
+        animator.SetFloat("Grip", gripValue);
+
+    }
+}
