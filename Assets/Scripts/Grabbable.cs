b8c0ec793f779654817bbec7ac9dc591c7572947
Guilherme Saturno <guilhermebsaturno@gmail.com>
2023-12-20 14:27:49 +0000

    Implement hand poses and tool for hand pose creation

---
+++ Assets/Scripts/Grabbable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Grabbable : XRGrabInteractable
+{
+    public HandPose leftHandPose;
+    public HandPose rightHandPose;
+}
---
+++ Assets/Scripts/HandPose.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HandPose", menuName = "ScriptableObjects/HandPose", order = 1)]
+public class HandPose : ScriptableObject
+{
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public List<Quaternion> fingerRotations;
+}
